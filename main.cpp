0f9ab74194b7af5d6e8ec0d72bea738afdbc26f1
wisefoolish <89657017+wisefoolish@users.noreply.github.com>
2023-04-04 13:53:28 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,44 @@
 #include"Graph.h"
-
 int main()
 {
-    initgraph(640, 480, SHOWCONSOLE);
-    setbkcolor(RGB(255, 255, 255));
-    cleardevice();
-    setlinecolor(RGB(0, 0, 0));
-    int x = 100, y = 100;
-    circle(100, 100, 20);
-    Sleep(1000);
-    circle(110, 110, 20);
-    Sleep(1000);
-    circle(120, 120, 20); 
-    system("pause");
+    initgraph(640, 480, DEFAULTWIN);
+    ExMessage msg;
+    BOOL isExit = FALSE;
+    BOOL isLPress = FALSE;
+    BOOL isRPress = FALSE;
+    while (!isExit)
+    {
+        msg = getmessage();
+        if (msg.message == WM_MOUSEMOVE)
+        {
+            if (isLPress)
+            {
+                putpixel(msg.x, msg.y, RGB(255, 0, 0));
+            }
+            else if (isRPress)
+            {
+                putpixel(msg.x, msg.y, RGB(0, 255, 0));
+            }
+            else putpixel(msg.x, msg.y, RGB(255, 255, 255));
+        }
+        else if (msg.message == WM_LBUTTONDOWN)
+        {
+            isLPress = TRUE;
+        }
+        else if (msg.message == WM_RBUTTONDOWN)
+        {
+            isRPress = TRUE;
+        }
+        else if (msg.message == WM_LBUTTONUP)
+        {
+            isLPress = FALSE;
+        }
+        else if (msg.message == WM_RBUTTONUP)
+        {
+            isRPress = FALSE;
+        }
+        else if (msg.message == WM_KEYDOWN && msg.vkcode == VK_ESCAPE) isExit = TRUE;
+    }
     closegraph();
     return 0;
 }
